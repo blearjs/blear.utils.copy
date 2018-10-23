4673f1166d8c1312798bac2c21000c93538900d1
#云淡然 <cloudcome@163.com>
2018-10-23 10:08:41 +0800

    ~ 复制功能，兼容桌面、手机

--- src/index.js
+++ src/index.js
@@ -6,7 +6,36 @@
 
 'use strict';
 
+var modi = require('blear.core.modification');
+var string = require('blear.utils.string');
 
+var inputEl = modi.create('input', {
+    style: {
+        position: 'absolute',
+        opacity: 0,
+        width: 1,
+        height: 1,
+        top: -999,
+        left: -999
+    }
+}, {
+    readOnly: true
+});
+modi.insert(inputEl);
+
+/**
+ * 复制【必须是由用户触发，否则无效】
+ * @param [text] {*} 待复制文本
+ * @returns {boolean} 是否复制成功
+ */
 module.exports = function (text) {
-
+    var value = string.ify(text);
+    inputEl.value = value;
+    inputEl.select();
+    inputEl.setSelectionRange(0, value.length);
+    try {
+        return document.execCommand('copy');
+    } catch (err) {
+        return false;
+    }
 };
--- test/test.index.js
+++ test/test.index.js
@@ -7,11 +7,22 @@
 
 'use strict';
 
-var index = require('../src/index.js');
+var copy = require('../src/index.js');
+var mod = require('blear.core.modification');
 
 describe('测试文件', function () {
-    it('exports', function (done) {
-        expect(index).toEqual('index');
-        done();
+
+    var buttonEl = mod.create('button');
+    buttonEl.innerHTML = '点击复制';
+    buttonEl.onclick = function () {
+        alert(copy(Math.random()));
+    };
+    mod.insert(buttonEl);
+
+    it('exports', function () {
+        var copied = copy('abc');
+        // 因为不是用户主动触发的，所以是 false
+        expect(copied).toBe(false);
     });
+
 });
